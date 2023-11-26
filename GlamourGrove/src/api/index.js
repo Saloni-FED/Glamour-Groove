200304939a24d1016eeb41e250e9e4e594b8d1b2
Saloni Pandey <salonipandey014@gmail.com>
2023-11-26 15:58:19 +0530

    configured .env file

--- GlamourGrove/src/api/index.js
+++ GlamourGrove/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const url = process.env.REACT_APP_API_URL;
+// const url = "http://localhost:3000";
+const url = process.env.REACT_APP_API_URL
 
 export const signup = (formData) => axios.post(`${url}/users/signup`, formData);
 export const signin = (formData) => axios.post(`${url}/users/signin`, formData);

20b078da3e937eea8df2acb41f39e6bb033f1390
Saloni Pandey <salonipandey014@gmail.com>
2023-11-26 17:18:41 +0530

    fixed routes

--- GlamourGrove/src/api/index.js
+++ GlamourGrove/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const url = "http://localhost:3000";
-// const url = process.env.REACT_APP_API_URL
+// const url = "http://localhost:3000";
+const url = "https://glamour-groove-server.onrender.com"
 
 export const signup = (formData) => axios.post(`${url}/users/signup`, formData);
 export const signin = (formData) => axios.post(`${url}/users/signin`, formData);

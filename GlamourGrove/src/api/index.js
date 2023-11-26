36f55ab731e8473198fa3169237e65ee942e61bd
Saloni Pandey <salonipandey014@gmail.com>
2023-11-26 17:29:27 +0530

    removed url

--- GlamourGrove/src/api/index.js
+++ GlamourGrove/src/api/index.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// const url = "http://localhost:3000";
 const url = "https://glamour-groove-server.onrender.com"
 
 export const signup = (formData) => axios.post(`${url}/users/signup`, formData);

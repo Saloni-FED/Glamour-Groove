d9c7a00791a1ee036813013912dcd22ba0bc89eb
Saloni Pandey <123414664+Saloni-FED@users.noreply.github.com>
2024-10-12 11:36:52 +0530

    Update Shop.jsx

--- GlamourGrove/src/Components/Shop.jsx
+++ GlamourGrove/src/Components/Shop.jsx
@@ -40,7 +40,7 @@ const Shop = () => {
     <>
       <section id="products" className="max-container max-sm:mt-12 ">
         <div className="flex flex-col justify-start gap-5">
-          <h2 className="text-4xl font-palanquin font-bold p-5 max-md:p-0 border-2">
+          <h2 className="text-4xl font-palanquin font-bold p-5 max-md:p-0">
             Our <span className="text-coral-red">Popular</span> Products
           </h2>
         </div>

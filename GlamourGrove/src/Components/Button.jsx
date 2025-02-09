d18a61a036a3b6f39e51e9c697d632b4964ee75c
Saloni Pandey <123414664+Saloni-FED@users.noreply.github.com>
2025-02-09 20:26:25 +0530

    Update Button.jsx

--- GlamourGrove/src/Components/Button.jsx
+++ GlamourGrove/src/Components/Button.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Button = () => {
   return (
     <Link to="/shop">
-      <button className=" flex justify-center items-center gap-4 px-7 py-4 border font-montserrat text-lf leading-none bg-coral-red text-white border-coral-red rounded-full">
+      <button className=" flex justify-center items-center gap-4 px-8 py-4 border font-montserrat text-lf leading-none bg-coral-red text-white border-coral-red rounded-full">
         Shop Now
         <FaArrowDown className="bg-white text-slate-gray px-1 rounded-full" />
       </button>
